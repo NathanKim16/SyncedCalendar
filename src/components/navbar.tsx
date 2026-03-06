b8389c3b0cb2e6e1fc48f689612f5942442ec1c0
NathanKim16 <nathan.kim@ufl.edu>
2026-03-06 13:09:32 -0500

    Added Tailwind CSS and created a navbar component.

---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+
+
+
+function NavBar() {
+  //Navbar for the calendar app
+    return (
+        <nav className="w-full fixed top-0 left-0 z-50 bg-white">
+            <div className="text-3xl font-bold">
+                <h1>Synced Calendar!</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
