1b52f509bd06816009fcb8208ea82d8d92764b3c
J-Wuuu <jwu651390@gmail.com>
2026-04-20 17:05:50 -0400

    connect invite code

--- src/components/feature/CodeInvite.js
+++ src/components/feature/CodeInvite.js
@@ -4,34 +4,38 @@ import {email} from "../Login"
 import {joinCode} from "../navbar"
 import {collection, query, where, onSnapshot, doc, addDoc} from "../../firebase/firestore";
 
-//calendar collection
-const calendarCollection = collection(db, 'calendars')
+export function inviteCodeFunction(joinCode){
+    //calendar collection
+    const calendarCollection = collection(db, 'calendars')
 
-//real time calendar collection data
-let calendars = []
-onSnapshot(calendarCollection, (snapshot) => {
-    snapshot.docs.forEach((doc) => {
-        calendars.push( doc.id)
-    })
-    console.log(calendars)
-})
-
-//go through calendar to find the calendar that matches the invite code
-//if found, add new membership to the calendar
-const calendarId = ""
-calendars.forEach((cal) => {
-    const lastFive = cal.substring(cal.length - 5);
-    if(lastFive == joinCode){
-        calendarId = cal;
-        addDoc(collection(db, "memberships"), {
-            cal_id: calendarId,
-            role: "member",
-            user_id: userId
+    //real time calendar collection data
+    let calendars = []
+    onSnapshot(calendarCollection, (snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            calendars.push(doc.id)
         })
-    }
-    else{
-        console.log(error)
-    }
-})
+    })
+
+    //go through calendar to find the calendar that matches the invite code
+    //if found, add new membership to the calendar
+    const calendarId = ""
+    calendars.forEach((cal) => {
+        const lastFive = cal.substring(cal.length - 5)
+        if(lastFive == joinCode){
+            calendarId = cal
+            addDoc(collection(db, "memberships"), {
+                cal_id: calendarId,
+                role: "member",
+                user_id: userId
+            })
+            return true
+        }
+        else{
+            return false
+        }
+    })
+    return false
+}
+
 
 
